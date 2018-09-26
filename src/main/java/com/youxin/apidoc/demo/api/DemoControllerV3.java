b57444f7c17dd21f61a40f4c23b67f7dfb3649eb
xingliang <xingliang@youxin.com>
2018-09-26 14:27:33 +0800

    add new version of user api.

---
+++ src/main/java/com/youxin/apidoc/demo/api/DemoControllerV3.java
@@ -0,0 +1,44 @@
+package com.youxin.apidoc.demo.api;
+
+import com.youxin.apidoc.demo.dto.UserDTO;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usersV2")
+public class DemoControllerV3 {
+
+    /**
+     *
+     * @api {post} /usersV2 增加用户
+     * @apiName addUser
+     * @apiGroup 用户
+     *
+     * @apiParam {String} id 用户名,长度0-100
+     * @apiParamExample {json} Request-Example:
+     * {
+     *     "name": "johny",
+     *      "age": 24
+     * }
+     *
+     * @apiSuccess {Number} id   Users unique ID.
+     * @apiSuccess {String} name   User name.
+     * @apiSuccess {Number} age   User age.
+     *
+     * @apiSuccessExample Success-Response:
+     *  HTTP/1.1 200 OK
+     *  {
+     *      "id": 1,
+     *      "name": "johny",
+     *      "age": 24
+     *  }
+     *
+     * @apiVersion 0.0.2
+     */
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public UserDTO addUser(UserDTO userDTO) {
+        return userDTO;
+    }
+}
